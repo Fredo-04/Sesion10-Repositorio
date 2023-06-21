9ac5a7c1471faa96d08ff624100bea6405445da8
Fredo <fredsotoucsm@gmail.com>
2023-06-20 23:51:05 -0500

    avance 2

--- Ejercicios/ejer3.cpp
+++ Ejercicios/ejer3.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+class Persona{
+    private: 
+        string nombre;
+        int edad;
+        string direccion;
+    public:
+        Persona(string nombre){this->nombre=nombre;}
+        void setEdad(int ed){
+            edad=ed;
+        }
+        void setDireccion(string dir){
+            direccion=dir;
+        }
+        string getNombre(){return nombre;}
+        int getEdad(){return edad;}
+        string getDireccion(){return direccion;}
+
+};
